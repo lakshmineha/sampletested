0c6bb7f16eff71de4798ab5300d120baa53655fd
lakshmineha <srilaxmi550@gmail.com>
2023-12-29 12:48:17 +0530

    update ine

--- sampletested/src/sampletest/hello1.java
+++ sampletested/src/sampletest/hello1.java
@@ -2,7 +2,7 @@ package sampletest;
 
 public class hello1 {
 	public static void main(String[] args) {
-		System.out.println("hellotestingteam1234");
+		System.out.println("hellotestingteam1234test");
 		
 	}
 
