c7c9f94e24ebd978db0681eb65fe20992602cfd7
lakshmineha <srilaxmi550@gmail.com>
2023-12-29 12:36:04 +0530

    commied

---
+++ sampletested/src/sampletest/hello1.java
@@ -0,0 +1,9 @@
+package sampletest;
+
+public class hello1 {
+	public static void main(String[] args) {
+		System.out.println("hellotestingteam1234");
+		
+	}
+
+}
---
+++ sampletested/src/sampletest/sampledemo.java
@@ -0,0 +1,8 @@
+package sampletest;
+
+public class sampledemo {
+	public static void main(String[] args) {
+		System.out.println("testing1234");
+	}
+
+}
